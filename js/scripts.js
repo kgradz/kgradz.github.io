0971fb649883745a77f040352f9ee7bfdd1f8213
kgradz <kgradz@gmail.com>
2017-01-26 11:10:07 +0100

    burger menu v1

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function() {
-   var isExpanded = hamburger.getAttribute("aria-expanded");
-   console.log(isExpanded);
-   if (isExpanded === "false") {
-      document.querySelector(".page-header").classList.add("nav-opened");
-      hamburger.setAttribute("aria-expanded", "true");
-   } else {
-      document.querySelector(".page-header").classList.remove("nav-opened");
-      hamburger.setAttribute("aria-expanded", "false");
-   }
-}, false);
-hamburger.setAttribute("role", "button");
-hamburger.setAttribute("aria-controls", "navigation");
-hamburger.setAttribute("aria-expanded", "false");
-document.body.classList.add("js");
+// $(document).ready(function(){
+// 	$('#navIcon').click(function(){
+// 		$(this).toggleClass('open');
+// 	});
+// });
